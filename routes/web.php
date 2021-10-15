4f2b10bf1f868c67435795c9b266266a5dc40096
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2021-10-15 20:26:26 +0200

    Paso 3

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,33 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('usuarios', function () {
+    return '<h1>Usuarios/as/es</h1>';
+});
+
+/*Route::get('usuarios/detalles', function () {
+    return 'mostrando detalles del usuario: ' . $_GET['id'];
+});*/
+
+Route::get('usuarios/nuevo', function () {
+    return 'Creando un nuevo usuario';
+});
+
+Route::get('usuarios/{id}', function ($id) {
+    return 'Mostrando detalles del usuario: ' . $id;
+})->where('id', '[0-9]+');
+
+Route::get('saludo/{name}/{nickname?}', function ($name, $nickname = null) {
+    /*if ($nickname) {
+        return 'Bienvenido ' . $name . '. Tu apodo es ' . $nickname;
+    }
+    return 'Bienvenido ' . $name . '.';*/
+
+    return $nickname
+        ? 'Bienvenido ' . $name . '. Tu apodo es ' . $nickname
+        : 'Bienvenido ' . $name . '.';
+
 });
