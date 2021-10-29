8bdf7526f230be653a4b75949fa864b616018484
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2021-10-29 19:21:55 +0200

    Fin paso 30

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -5,14 +5,31 @@
 @section('content')
     <h1>Crear nuevo usuario</h1>
 
+    @if($errors->any())
+        <div class="alert alert-danger">
+            <h6>Por favor, corrige los siguientes errores</h6>
+<!--            <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>-->
+        </div>
+    @endif
+
     <form action="{{ route('users.store') }}" method="POST">
         {{ csrf_field() }}
 
         <label for="name">Nombre:</label>
-        <input type="text" name="name">
+        <input type="text" name="name" value="{{ old('name') }}">
+        @if($errors->has('name'))
+            <p>{{ $errors->first('name') }}</p>
+        @endif
         <br>
         <label for="email">Correo Electrónico</label>
-        <input type="email" name="email">
+        <input type="email" name="email" value="{{ old('email') }}">
+        @if($errors->has('email'))
+            <p>{{ $errors->first('email') }}</p>
+        @endif
         <br>
         <label for="password">Contraseña: </label>
         <input type="password" name="password">
